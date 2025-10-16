fe1cbfbbc5ccc598881b616b4d44c6e9b217f857
hadeer <hadeeressam21@gmail.com>
2025-10-16 16:20:33 +0300

    + add a Constant OBJECT includes the API, Image_PAI, and the MAX item size

---
+++ app/src/main/java/com/hadeer/jetpackcomposepokemon/data/remote/Constant.kt
@@ -0,0 +1,7 @@
+package com.hadeer.jetpackcomposepokemon.data.remote
+
+object Constant {
+    const val API_SERVICE = "https://pokeapi.co/api/v2/"
+    const val PAGE_SIZE = 300
+    const val POKEMON_IMAGE_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
+}
