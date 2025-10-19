e17c65d6e6e2d0d2a93593504a46f34251740fb5
hadeer <hadeeressam21@gmail.com>
2025-10-19 16:47:46 +0300

    $ move some file with functionality to Util package

--- app/src/main/java/com/hadeer/jetpackcomposepokemon/util/NetworkResponse.kt
+++ app/src/main/java/com/hadeer/jetpackcomposepokemon/util/NetworkResponse.kt
@@ -1,4 +1,4 @@
-package com.hadeer.jetpackcomposepokemon.data.remote
+package com.hadeer.jetpackcomposepokemon.util
 
 import java.io.IOException
 
@@ -6,7 +6,7 @@ sealed class Resource<T>(val data: T? = null, val message : String? = null){
 
     class Success<T>(data: T?) : Resource<T>(data)
     class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
-    class Loading<T>(data: T? = null) : Resource<T>(data)
+    class Loading<T>() : Resource<T>()
 }
 
 
